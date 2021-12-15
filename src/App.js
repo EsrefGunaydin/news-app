17a15cee83e658840c3fcbe4945218448e8bb2fc
Esref Gunaydin <esrefgunaydin@gmail.com>
2021-12-15 15:22:25 -0800

    app updated

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import styled from "styled-components";
 import PulseLoader from "react-spinners/PulseLoader";
 
 export default function App() {
-  const { isLoading, isError, data } = useQuery("news", () =>
+  const { isLoading, isError, data, error } = useQuery("news", () =>
     axios.get(
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=138092459a554575a92f412630b423bb"
     )
   );
 
   if (isLoading) return <PulseLoader />;
-  if (isError) return <Title>Error!</Title>;
+  if (isError) return <Title>Error!: {error}</Title>;
   return (
     <>
       <NewsCard />
